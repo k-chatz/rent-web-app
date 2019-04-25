8f507a1859789e99270ea78addc5cd6e140dbb2a
Costas Chatzopoulos <kwstarikanos@gmail.com>
2019-04-25 22:03:34 +0300

    Management module: New users-insert-new component. ✔

---
+++ src/app/management/modules/users/users-insert-new/users-insert-new.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-users-insert-new',
+  templateUrl: './users-insert-new.component.html',
+  styleUrls: ['./users-insert-new.component.scss']
+})
+export class UsersInsertNewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
