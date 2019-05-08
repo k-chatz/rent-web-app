783ee435d2901746d6b1af38a93a7b7ee7771a44
Costas Chatzopoulos <kwstarikanos@gmail.com>
2019-05-08 03:30:34 +0300

    Update getAll users request to accept filter params.
    Co-authored-by: kwstarikanos <kwstarikanos@gmail.com>
    Co-authored-by: petethunder <morfiristhunder@hotmail.gr>

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -1,15 +1,17 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
 import {User} from '../models/user';
 import {environment} from '../../../environments/environment';
+import {PagedResponse} from '../models/payload/PagedResponse';
 
 @Injectable({providedIn: 'root'})
 export class UserService {
   constructor(private http: HttpClient) {
   }
 
-  getAll() {
-    return this.http.get<User[]>(`${environment.apiRoot}/users`);
+  getAll(filters) {
+    const params = new HttpParams({fromObject: filters});
+    return this.http.get<PagedResponse<User>>(`${environment.apiRoot}/users`, {params});
   }
 
   getById(id: string) {
