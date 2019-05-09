8fcb672b3cc07c69a4c313ba9600e5bbd50e1a95
Costas Chatzopoulos <kwstarikanos@gmail.com>
2019-05-09 06:59:03 +0300

    Create filter widget in search results page.
    Co-authored-by: kwstarikanos <kwstarikanos@gmail.com>
    Co-authored-by: petethunder <morfiristhunder@hotmail.gr>

--- src/app/main/modules/search-results/components/filters/filters.component.ts
+++ src/app/main/modules/search-results/components/filters/filters.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+import {ChangeContext, LabelType, Options, PointerType} from 'ng5-slider';
 
 @Component({
   selector: 'app-filters',
@@ -7,9 +10,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FiltersComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+  ) {
+    this.form = this.fb.group(
+      {
+        slider: ['', {
+          validators: []
+        }],
+        petsAllowed: ['', {
+          validators: []
+        }],
+        freeWifi: ['', {
+          validators: []
+        }],
+        view: ['', {
+          validators: []
+        }],
+      }
+    );
+  }
+
+  form: FormGroup;
+  progress: boolean;
+  @Input() minPrice: number;
+  @Input() maxPrice: number;
+
+  minValue: number;
+  maxValue: number;
+  options: Options;
+
+  logText = '';
+  checked = true;
 
   ngOnInit() {
+    this.options = {
+      floor: this.minPrice,
+      ceil: this.maxPrice,
+      translate: (value: number, label: LabelType): string => {
+        switch (label) {
+          case LabelType.Low:
+            return '<b>Min price:</b> $' + value;
+          case LabelType.High:
+            return '<b>Max price:</b> $' + value;
+          default:
+            return '$' + value;
+        }
+      }
+    };
+    this.minValue = this.minPrice;
+    this.maxValue = this.maxPrice;
+  }
+
+  onUserChangeEnd(changeContext: ChangeContext): void {
+    console.log('end', changeContext);
+    // TODO change router query params
+  }
+
+  submit(value: any) {
   }
 
 }
