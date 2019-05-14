9fec8eae46a4e906ba158c1de0ef4ea17acee793
LSmyrnaios <lsmyrnaios@gmail.com>
2019-05-14 20:47:38 +0300

    Update Profile to show the real information of the logged-in user.

--- src/app/main/modules/profile/profile.component.ts
+++ src/app/main/modules/profile/profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {User} from '../../../shared/models/user';
 import {Title} from '@angular/platform-browser';
+import {AuthenticationService} from '../../../shared/services/authentication.service';
+import {Session} from '../../../shared/models/session';
 
 @Component({
   selector: 'app-profile',
@@ -8,22 +9,22 @@ import {Title} from '@angular/platform-browser';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
-  username = 'antoni4040';
-  name = 'Antoni';
-  surname = 'Karvelas';
-  birthday = '03/05/1998';
 
   editing = false;
 
-  private user: User;
+  public session: Session;
 
   public constructor(
-    private titleService: Title
+    private titleService: Title,
+    private auth: AuthenticationService
   ) {
     titleService.setTitle('Profile');
   }
 
   ngOnInit() {
+    this.auth.session$.subscribe( session => {
+      this.session = session;
+    });
   }
 
 }
