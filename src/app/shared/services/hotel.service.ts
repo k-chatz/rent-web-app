e32425dc9f4f878d5837f94d008a300c53f15d6a
Costas Chatzopoulos <kwstarikanos@gmail.com>
2019-06-17 22:08:22 +0300

    update hotel service

--- src/app/shared/services/hotel.service.ts
+++ src/app/shared/services/hotel.service.ts
@@ -1,11 +1,12 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpParams} from '@angular/common/http';
 import {environment} from '../../../environments/environment';
-import {Hotel} from '../models/hotel';
+import {Hotel} from '../models/Hotel';
 import {Observable} from 'rxjs';
 import {map} from 'rxjs/operators';
 import * as moment from 'moment';
 import {SearchResults} from '../models/payload/SearchResults';
+import {Room} from '../models/Room';
 
 @Injectable({
   providedIn: 'root'
@@ -19,6 +20,15 @@ export class HotelService {
 
   getHotelDetails(hotelID): Observable<Hotel> {
     return this.http.get<Hotel>(environment.apiRoot + '/hotels/' + hotelID);
+  }
+
+  getHotelRooms(hotelID): Observable<Room> {
+    return this.http.get<Room>(environment.apiRoot + '/hotels/' + hotelID + '/rooms');
+  }
+
+  getHotelByOwner(filters) {
+    const params = new HttpParams({fromObject: filters});
+    return this.http.get<Room>(environment.apiRoot + '/hotels/', {params});
   }
 
   search(filters) {
