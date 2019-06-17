a10bc61753d5522ccf349fba35d23701a09454ef
Costas Chatzopoulos <kwstarikanos@gmail.com>
2019-06-17 22:07:43 +0300

    business service

---
+++ src/app/shared/services/business.service.ts
@@ -0,0 +1,21 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {environment} from '../../../environments/environment';
+import {Business} from '../models/Business';
+import {PagedResponse} from '../models/payload/PagedResponse';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BusinessService {
+
+  constructor(
+    private http: HttpClient
+  ) {
+  }
+
+  getMyBusinesses(filters) {
+    const params = new HttpParams({fromObject: filters});
+    return this.http.get<PagedResponse<Business>>(environment.apiRoot + '/businesses/my', {params});
+  }
+}
