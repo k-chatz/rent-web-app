1da60bb16ca467b63e15019425baf575812f9424
Costas Chatzopoulos <kwstarikanos@gmail.com>
2019-06-19 00:30:43 +0300

    select distance radius input at search-widget

--- src/app/shared/components/search-widget/search-widget.component.ts
+++ src/app/shared/components/search-widget/search-widget.component.ts
@@ -12,6 +12,7 @@ export class SearchWidgetComponent implements OnInit {
   @Input() destination;
   @Input() latitude;
   @Input() longitude;
+  @Input() radius;
   @Input() visitors;
   @Input() startDate;
   @Input() endDate;
@@ -47,6 +48,9 @@ export class SearchWidgetComponent implements OnInit {
         lng: [null, {
           validators: [Validators.required]
         }],
+        radius: [null, {
+          validators: [Validators.required]
+        }],
         visitors: [null, {
           validators: [Validators.min(1), Validators.max(15)]
         }]
@@ -63,9 +67,10 @@ export class SearchWidgetComponent implements OnInit {
     this.form.get('visitors').setValue(this.visitors);
     this.form.get('lat').setValue(this.latitude);
     this.form.get('lng').setValue(this.longitude);
+    this.form.get('radius').setValue(this.radius);
   }
 
-  valuechange(value: string) {
+  onPlaceChange(value: string) {
     if (value === '') {
       this.form.get('lat').setValue(null);
       this.form.get('lng').setValue(null);
@@ -88,53 +93,11 @@ export class SearchWidgetComponent implements OnInit {
           destination: value.destination,
           lat: value.lat,
           lng: value.lng,
+          radius: value.radius,
           visitors: value.visitors,
         },
         queryParamsHandling: 'merge'
       });
   }
-
-  /*disabledStartDate(): boolean {
-    if (!this.startDate || !this.endDate) {
-      return false;
-    }
-    return this.endDate.getTime() > this.startDate.getTime();
-  }
-
-  disabledEndDate(): boolean {
-    if (!this.endDate || !this.startDate) {
-      return false;
-    }
-    return this.endDate.getTime() <= this.startDate.getTime();
-  }
-
-  onStartChange(date: Date): void {
-    this.form.get('daterangeGroup').get('startDate').setValue(date);
-  }
-
-  onEndChange(date: Date): void {
-    this.form.get('daterangeGroup').get('endDate').setValue(date);
-  }
-
-  handleStartOpenChange(open: boolean): void {
-    if (!open) {
-      this.endOpen = true;
-    }
-    // console.log('handleStartOpenChange', open, this.endOpen);
-  }
-
-  handleEndOpenChange(open: boolean): void {
-    // console.log(open);
-    this.endOpen = open;
-  }
-
-  get startDate() {
-    return this.form.get('daterangeGroup').get('startDate').value;
-  }
-
-  get endDate() {
-    return this.form.get('daterangeGroup').get('endDate').value;
-  }
-*/
 
 }
